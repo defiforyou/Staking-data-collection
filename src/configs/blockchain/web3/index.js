08de46e39613f5fa642243ca5b51580fada6635d
Brenn Hill <brennhill@gmail.com>
2021-09-06 22:36:31 +0700

    Enforces node 16

--- src/configs/blockchain/web3/index.js
+++ src/configs/blockchain/web3/index.js
@@ -1,6 +1,17 @@
 const Web3 = require('web3');
 require('dotenv').config()
 
+const checkNodeVersion = version => {
+    const versionRegex = new RegExp(`^${version}\\..*`);
+    const versionCorrect = process.versions.node.match(versionRegex);
+    if (!versionCorrect) {
+        throw Error(
+            `Running on wrong Nodejs version. Please upgrade the node runtime to version ${version}`
+        );
+    }
+};
+checkNodeVersion(16)
+
 const binanceProvider={
     MAINNET:[
         new Web3(process.env.WEB3),
