5c76d50e062e8f3df3284a0a0e466b21e2d24ef0
Brenn Hill <brennhill@gmail.com>
2021-09-08 21:45:20 +0700

    update naming of input CSV for additional clarity

--- src/services/dfy_collection.js
+++ src/services/dfy_collection.js
@@ -18,7 +18,7 @@ const waitFor = util.promisify(setTimeout);
 let blockStart = parseInt(process.env.BLOCK_START_DFY);
 let blockStop = parseInt(process.env.BLOCK_END);
 
-// Set of account of 
+// Set of account of
 let accountSet = new Set();
 
 let accountToBalanceMap = {};
@@ -37,7 +37,7 @@ async function writeToResultCSV(wallet_address, balance_amount) {
     await csvWriter.writeRecords(row);
 }
 
-// this func is for dumping the 
+// this func is for dumping the
 async function readCollectedData(path) {
 
     const readline = require("readline");
@@ -115,14 +115,14 @@ async function getBalancesOfAccountSet() {
                     console.log("result: " + wallet + " - " + balance.toFixed());
                     accountToBalanceMap[wallet] = balance;
                     break;
-                } 
+                }
                 catch (error) {
                     if (trial == 5) {
                         break;
                     }
-                } 
+                }
             }
-        }); 
+        });
         promise.then();
     }
     const sortedAccountToBalanceMap = new Map([...accountToBalanceMap.entries()].sort());
@@ -132,7 +132,7 @@ async function getBalancesOfAccountSet() {
 }
 
 async function scan() {
-    await readCollectedData('./export-tokenholders-for-contract-0xd98560689c6e748dc37bc410b4d3096b1aa3d8c2.csv');
+    await readCollectedData('./dfy_account_holders_asof_block-10714832.csv');
 
     // after running this loop we get the account set (holders list)
     while (true) {
@@ -161,7 +161,7 @@ async function scan() {
             console.error(e.message)
         }
     };
-    getBalancesOfAccountSet();
+    await getBalancesOfAccountSet();
 }
 
 scan();
